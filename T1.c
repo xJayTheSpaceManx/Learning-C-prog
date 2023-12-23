5df5cf97e3752973a619f556d18d2a66ced11570
xJayTheSpaceManx <154399137+xJayTheSpaceManx@users.noreply.github.com>
2023-12-22 19:19:45 -0500

    Create T1.c

---
+++ T1.c
@@ -0,0 +1,14 @@
+/*
+Name "Hello World"
+My first C program
+By JaytheSpaceMan on 12/21/2023
+*/
+#include <stdio.h> // include standerd lib
+
+int main(int argc, char *argv[]) //print "Hello World!" in terminal
+{
+  char helloWorld[12] = "Hello World!";
+
+  printf("%s", helloWorld);
+  return 0;
+}
